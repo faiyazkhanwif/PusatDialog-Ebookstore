2322450652ceea7cfe59c019ede20a82468c157c
faiyazkhanwif <faiyazkhanwif@gmail.com>
2021-01-04 23:54:13 +0800

    Navbar categories bug fixed

--- application/views/layouts/user_layout.php
+++ application/views/layouts/user_layout.php
@@ -11,7 +11,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     
     <!-- jQuery library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="<?= base_url('https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js'); ?>"></script>
     <!--==== CSS =====-->
 
     <!-- Bootstrap css -->
@@ -80,18 +80,32 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         </div>
     </div>
 </header>
-<!--============ Content-area ===========-->
-<div class="container">
-    <div class="row">
-        <div class="col-lg-12 col-md-12 col-sm-12" style="min-height: 500px">
-            <?php $this->load->view($user_view); ?>
+<main>
+    <!--============ Content-area ===========-->
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12" style="min-height: 500px">
+                <?php $this->load->view($user_view); ?>
+            </div>
         </div>
     </div>
-</div>
+</main>
+
+<footer>
+    <!--============== Footer Area ==============-->
+    <div>
+        <?php $this->load->view('include/footer'); ?>
+    </div>
+</footer>
+
+<script type="text/javascript" src="<?= base_url('resource/js/jquery-3.2.1.slim.min.js'); ?>"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="<?= base_url('resource/js/bootstrap.min.js'); ?>"></script>
+<!-- Popper JS -->
+<script src="<?= base_url('https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js'); ?>"></script>
 
 
+</body>
 
-<!--============== Footer Area ==============-->
-<div>
-    <?php $this->load->view('include/footer'); ?>
-</div>
+</html>
