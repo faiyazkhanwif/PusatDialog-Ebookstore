4e69963fdf279663cbadd714f3cc68cbc6e0d686
faiyazkhanwif <faiyazkhanwif@gmail.com>
2021-03-13 23:29:14 +0800

    Minor edit in admin nav

--- application/views/admin/admin_nav.php
+++ application/views/admin/admin_nav.php
@@ -5,7 +5,7 @@
         <ul>
             <li><a href="<?= base_url()?>admin"><i class="fas fa-home"></i> Dashboard</a></li>
             <li><a href="<?= base_url()?>admin/category"><i class="far fa-list-alt"></i> Category</a></li>
-            <li><a href="<?= base_url()?>admin/books"><i class="fas fa-book"></i> Books</a></li>
+            <li><a href="<?= base_url()?>admin/books"><i class="fas fa-book"></i> E-Books</a></li>
             <li><a href="<?= base_url()?>admin/ebooks"><i class="fas fa-chalkboard-teacher"></i> E-books for Borrow</a></li>
             <li><a href="<?= base_url()?>admin/orders"><i class="fas fa-cart-arrow-down"></i> Orders</a></li>
             <li><a href="<?= base_url()?>admin/allusers"><i class="fas fa-users"></i> Users</a></li>
